e95398db4be8dee45172d9768c288fbb3b2ccbd1
Somtok Emmanuel <somtokemmanuel@gmail.com>
2024-11-20 07:11:25 +0100

    Add files via upload

---
+++ function.js
@@ -0,0 +1,19 @@
+// An arrow function is  a concise way to write.
+// functions in javaScript,
+
+// Welcome function
+const user = (name) =>
+    `welcome to my website, ${name}`;
+console.log(user("martinz."));
+
+// sum of 3 numbers
+const sum = (a, b, c) => a + b + c;
+console.log(sum(5, 10, 15));
+
+// modulus of 2 numbers
+const modulus = (a, b) => a % b;
+console.log(modulus(11, 3));
+
+
+
+
